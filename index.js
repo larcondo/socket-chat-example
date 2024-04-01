0adf1aa154515e9bdffc84d3500e9a09fcc807aa
larcondo <larcondo90@gmail.com>
2024-04-01 19:52:31 -0300

    step2: serving HTML

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { join } = require('node:path');
+
 const PORT = 3000;
+
 const app = express();
 const server = createServer(app);
 
-const htmlContent = `
-  <h1>Hello World!</h1>
-  <p>socket.io application example</p>
-`
-
 app.get('/', (req, res) => {
-  res.send(htmlContent);
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 server.listen(PORT, () => {
