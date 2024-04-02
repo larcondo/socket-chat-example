e7d9edd44c1170eed98a530b2b160892e8c3d71c
larcondo <larcondo90@gmail.com>
2024-04-01 21:05:33 -0300

    step7: server delivery

--- index.js
+++ index.js
@@ -2,30 +2,76 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
 const PORT = 3000;
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {}
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+async function main() {
+  // open the database file
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
+  // create our 'message' table (you can ignore the 'client_offset' column for now)
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+    );
+  `);
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
-});
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+
+  io.on('connection', async (socket) => {
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  
+    socket.on('chat message', async (msg) => {
+      let result;
+      try {
+        // store the message in the database
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch(e) {
+        // TODO handle the failure
+        console.log('FAIL: store message fail!');
+        return;
+      }
+
+      // include the offset with the message
+      io.emit('chat message', msg, result.lastID);
+    });
+
+    if (!socket.recovered) {
+      // if the connection state recovery was not successful
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          }
+        )
+      } catch(e) {
+        console.log('FAIL: state recovery fail!');
+        // something went wrong
+      }
+    }
+  });
+  
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`);
+  });
+}
+
+main();
